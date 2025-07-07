7de563df43afa39de1e81d35224fb5c602852fa9
Atharva Gadekar. <167705336+atharva154@users.noreply.github.com>
2025-07-07 16:13:45 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/:path*' // Flask backend
+      }
+    ]
+  }
+};
+
+export default nextConfig;
